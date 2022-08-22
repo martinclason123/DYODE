8465f49a10ed8360d7761ea8ae65c39f82e2115d
martinclason123 <martinclason123@yahoo.com>
2022-08-22 07:40:19 -0400

    added newsletter component

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -5,11 +5,34 @@ const Newsletter = ({ message, blurb, actionMessage }) => {
   return (
     <div className="newsletter">
       <div className="newsletter__header">
-        <img className="newsletter__img" src={icons.Newsletter} />
+        <img
+          className="newsletter__img"
+          src={icons.Newsletter}
+          alt="email icon"
+        />
         <h1 className="newsletter__message">{message}</h1>
       </div>
-      <p className="newsletter__blurb">{blurb}</p>
-      <button className="newsletter__button">{actionMessage}</button>
+      <div className="newsletter__subscribe-mobile">
+        <p className="newsletter__blurb-mobile">{blurb}</p>
+        <button className="newsletter__submit-mobile">{actionMessage}</button>
+      </div>
+      <div className="newsletter__subscribe-desktop">
+        <div className="newletter__form-desktop">
+          <div className="newsletter__blurb-desktop">
+            <p>{blurb}</p>
+          </div>
+          <div className="newsletter__submit-desktop">
+            <input
+              className="newsletter__input-desktop"
+              type="text"
+              placeholder="Enter Your Email Address"
+            />
+            <button className="newsletter__button-desktop">
+              {actionMessage}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
